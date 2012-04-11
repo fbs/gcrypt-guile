cbe14c26a53126af719b86cdc7e2f75cf8c447b6
bas <fbs.devv@gmail.com>
2012-04-11 22:18:49 +0200

    Changed from string to bytevector

--- src/hash.c
+++ src/hash.c
@@ -21,32 +21,51 @@
 #include <libguile.h>
 
 SCM
-scm_gcrypt_quick_hash(SCM scm_str, SCM scm_algo)
-#define FUNC_NAME "gcrypt:quick-hash"
+scm_gcrypt_quick_hash(SCM data_bv, SCM scm_algo)
+#define FUNC_NAME "gcrypt_quick-hash"
 {
-  SCM_ASSERT(scm_string_p(scm_str), scm_str, SCM_ARG1, FUNC_NAME);
+  SCM_ASSERT(scm_bytevector_p(data_bv), data_bv, SCM_ARG1, FUNC_NAME);
   SCM_ASSERT(scm_integer_p(scm_algo), scm_algo, SCM_ARG2, FUNC_NAME);
 
   int		algo	= scm_to_int(scm_algo);
-  char *	str	= scm_to_locale_string(scm_str);
-  int		dig_len = gcry_md_get_algo_dlen(algo);
-  SCM		bv	= scm_c_make_bytevector (dig_len);
-  char *	digest	= (char *) SCM_BYTEVECTOR_CONTENTS(bv);
-  
+  unsigned int  dlen    ;
+  SCM		hash_bv	;
   gcry_error_t  err	= 0;
   
-  err = gcry_md_test_algo(algo);  
+  err = gcry_md_test_algo(algo);
+  
   if ( err )
     {
-      SCM		err_msg;
+      SCM err_msg;
       err_msg = scm_from_latin1_string ("Invalid digest algorithm: ~A");
-      scm_error_scm (scm_misc_error_key, scm_from_latin1_string("gcrypt:quick-hash"),
-		     err_msg, scm_list_1(scm_algo), SCM_BOOL_F);
+      scm_error_scm (scm_misc_error_key,
+		     scm_from_latin1_string("gcrypt:quick-hash"),
+		     err_msg,
+		     scm_list_1(scm_algo),
+		     SCM_BOOL_F);
+    }
+
+  dlen    = gcry_md_get_algo_dlen (algo);
+  
+  if ( dlen < 4 ) 
+    {
+      SCM err_msg;
+      err_msg = scm_from_latin1_string ("Invalid digest length: ~A");
+      scm_error_scm (scm_misc_error_key,
+		     scm_from_latin1_string("gcrpt:quick-hash"),
+		     err_msg,
+		     scm_list_1(scm_from_int (dlen)),
+		     SCM_BOOL_F);
     }
   
-  gcry_md_hash_buffer (algo, digest, str, strlen(str));
+  hash_bv = scm_c_make_bytevector (dlen);
+  //gcry_md_hash (algo, digest, buffer, length)
+  gcry_md_hash_buffer (algo,
+		       (void *) SCM_BYTEVECTOR_CONTENTS(hash_bv),
+		       (void *) SCM_BYTEVECTOR_CONTENTS(data_bv),
+		       SCM_BYTEVECTOR_LENGTH(data_bv));
 
-  return bv;
+  return hash_bv;
 }
 
 #undef FUNC_NAME
